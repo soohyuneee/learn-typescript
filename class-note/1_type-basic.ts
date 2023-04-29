ba548fae9de701d50fca64974849b5a789b9594a
soohyuneee <tlstngus95@naver.com>
2023-04-29 23:45:41 +0900

    타입스크립트 기초 - 변수와 함수 타입 정의하기

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,30 @@
+// JS 문자열 선언
+// let str = 'hello';
+
+// TS 문자열 선언
+let str: string = 'hello';
+
+// TS 숫자
+let num: number = 20;
+
+// TS 배열
+let arr: Array<number> = [1, 2, 3];
+let heros: Array<string> = ['Capt', 'Thor', 'Hulk'];
+let item: number[] = [1, 2, 3];
+
+// TS 튜플
+let address: [string, number] = ['seoul', 100];
+
+// TS 객체
+let obj: object = {};
+// let person: object = {
+// 	name: 'soo',
+// 	age: 100,
+// };
+let person: {name: string; age: number} = {
+	name: 'soo',
+	age: 100,
+};
+
+// TS 진위값
+let show: boolean = true;
