8ba74f7cc28f8f32d5ea775c42ede06255565b0e
soohyuneee <tlstngus95@naver.com>
2023-05-01 00:22:14 +0900

    제네릭 적용

--- example/dropdown-generic.ts
+++ example/dropdown-generic.ts
@@ -1,26 +1,36 @@
-const emails = [
-  { value: 'naver.com', selected: true },
-  { value: 'gmail.com', selected: false },
-  { value: 'hanmail.net', selected: false },
+interface Dropdown<T> {
+	value: T;
+	selected: boolean;
+}
+
+interface Itme<T> {
+	value: T;
+	selected: boolean;
+}
+
+const emails: Dropdown<string>[] = [
+	{ value: 'naver.com', selected: true },
+	{ value: 'gmail.com', selected: false },
+	{ value: 'hanmail.net', selected: false },
 ];
 
-const numberOfProducts = [
-  { value: 1, selected: true },
-  { value: 2, selected: false },
-  { value: 3, selected: false },
+const numberOfProducts: Dropdown<number>[] = [
+	{ value: 1, selected: true },
+	{ value: 2, selected: false },
+	{ value: 3, selected: false },
 ];
 
-function createDropdownItem(item) {
-  const option = document.createElement('option');
-  option.value = item.value.toString();
-  option.innerText = item.value.toString();
-  option.selected = item.selected;
-  return option;
+function createDropdownItem(item: Dropdown<string> | Dropdown<number>) {
+	const option = document.createElement('option');
+	option.value = item.value.toString();
+	option.innerText = item.value.toString();
+	option.selected = item.selected;
+	return option;
 }
 
 // NOTE: 이메일 드롭 다운 아이템 추가
 emails.forEach(function (email) {
-  const item = createDropdownItem(email);
-  const selectTag = document.querySelector('#email-dropdown');
-  selectTag.appendChild(item);
+	const item = createDropdownItem(email);
+	const selectTag = document.querySelector('#email-dropdown');
+	selectTag.appendChild(item);
 });
